47d25af951556cccbf63a7c8c7ebcd3948162259
root <gonzaloahuerta@gmail.com>
2019-10-23 22:02:39 -0300

    Actualizo tabla viaje con origen y destino + inserts de viajes

--- bdGauchoRocket.sql
+++ bdGauchoRocket.sql
@@ -1,148 +1,486 @@
-Create database gaucho_rocket;
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 24-10-2019 a las 02:58:16
+-- Versión del servidor: 10.4.6-MariaDB
+-- Versión de PHP: 7.3.8
 
-create table lugar(
-id_lugar int,
-nombre_lugar varchar(20),
-primary key (id_lugar)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-create table tipo_viaje(
-id_tipo_viaje int,
-nombre_tipo_viaje varchar(20),
-primary key(id_tipo_viaje)
-);
 
-create table turno_hospital(
-id_hospital int,
-nombre_hospital varchar(35),
-turnos int,
-primary key(id_hospital)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-create table estado_reserva(
-id_estado_reserva int,
-estado varchar(15),
-primary key(id_estado_reserva)
-);
+--
+-- Base de datos: `gaucho_rocket`
+--
 
-create table nave(
-id_nave int,
-tipo_nave varchar(20),
-primary key(id_nave)
-);
+-- --------------------------------------------------------
 
-create table cabina(
-id_cabina int,
-tipo_cabina varchar(20),
-cantidad int,
-primary key(id_cabina)
-);
+--
+-- Estructura de tabla para la tabla `cabina`
+--
 
-create table servicio(
-id_servicio int,
-tipo_sevicio varchar(20),
-primary key(id_servicio)
-);
+CREATE TABLE `cabina` (
+  `id_cabina` int(11) NOT NULL,
+  `tipo_cabina` varchar(20) DEFAULT NULL,
+  `cantidad` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-create table viaje(
-id_viaje int,
-salida_viaje datetime,
-llegada_viaje datetime,
-duracion time,
-precio int,
-id_tipo_viaje int,
-id_lugar int,
-primary key(id_viaje),
-foreign key(id_tipo_viaje) references tipo_viaje(id_tipo_viaje),
-foreign key(id_lugar) references lugar(id_lugar)
-);
+-- --------------------------------------------------------
 
-create table nave_tiene_cabina(
-id_nave int,
-id_cabina int,
-primary key(id_nave,id_cabina),
-foreign key(id_nave) references nave(id_nave),
-foreign key(id_cabina) references cabina(id_cabina)
-);
+--
+-- Estructura de tabla para la tabla `estado_fisico`
+--
 
-create table reserva(
-id_reserva int,
-vencimiento_reserva datetime,
-id_estado_reserva int,
-primary key (id_reserva),
-foreign key (id_estado_reserva) references estado_reserva(id_estado_reserva)
-);
+CREATE TABLE `estado_fisico` (
+  `id_estado_fisico` int(11) NOT NULL,
+  `numero` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-create table viaje_hecho_por(
-id_reserva int,
-id_viaje int,
-id_nave int,
-primary key(id_reserva,id_viaje,id_nave),
-foreign key (id_reserva) references reserva(id_reserva),
-foreign key (id_viaje) references viaje(id_viaje),
-foreign key (id_nave) references nave(id_nave)
-); 
+-- --------------------------------------------------------
 
-create table reserva_cabina(
-id_reserva int,
-id_cabina int,
-primary key(id_reserva,id_cabina),
-foreign key(id_reserva)references reserva(id_reserva),
-foreign key(id_cabina)references cabina(id_cabina)
-);
+--
+-- Estructura de tabla para la tabla `estado_reserva`
+--
 
-create table reserva_servicio(
-id_reserva int,
-id_servicio int,
-primary key(id_reserva,id_servicio),
-foreign key(id_reserva) references reserva(id_reserva),
-foreign key(id_servicio) references servicio(id_servicio)
-);
+CREATE TABLE `estado_reserva` (
+  `id_estado_reserva` int(11) NOT NULL,
+  `estado` varchar(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-create table tipo_usuario(
-id_tipo_usuario int,
-descripcion varchar(20),
-primary key(id_tipo_usuario)
-);
+-- --------------------------------------------------------
 
-create table usuario(
-id_usuario int,
-nombre varchar(20),
-apellido varchar(25),
-email varchar(40),
-contraseña varchar(40),
-id_tipo_usuario int,
-primary key (id_usuario),
-foreign key(id_tipo_usuario) references tipo_usuario(id_tipo_usuario) 
-);
+--
+-- Estructura de tabla para la tabla `lugar`
+--
 
-create table usuario_hace_reserva(
-id_reserva int,
-id_usuario int,
-primary key(id_reserva,id_usuario),
-foreign key(id_reserva) references reserva(id_reserva),
-foreign key(id_usuario) references usuario(id_usuario)
-);
+CREATE TABLE `lugar` (
+  `id_lugar` int(11) NOT NULL,
+  `nombre_lugar` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-create table estado_fisico(
-id_estado_fisico int,
-numero int,
-primary key(id_estado_fisico)
-);
+--
+-- Volcado de datos para la tabla `lugar`
+--
 
-create table viaje_puede_ser_hecho_por(
-id_estado_fisico int,
-id_viaje int,
-primary key(id_estado_fisico,id_viaje),
-foreign key (id_estado_fisico)references estado_fisico(id_estado_fisico),
-foreign key (id_viaje) references viaje(id_viaje)
-);
+INSERT INTO `lugar` (`id_lugar`, `nombre_lugar`) VALUES
+(1, 'Saturno'),
+(2, 'Neptuno'),
+(3, 'Jupiter'),
+(4, 'Tierra');
 
-create table pasajero(
-id_usuario int,
-id_estado_fisico int,
-id_hospital int,
-foreign key(id_usuario) references usuario(id_usuario),
-foreign key(id_estado_fisico) references estado_fisico(id_estado_fisico),
-foreign key(id_hospital) references turno_hospital(id_hospital)
-);
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `nave`
+--
+
+CREATE TABLE `nave` (
+  `id_nave` int(11) NOT NULL,
+  `tipo_nave` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `nave_tiene_cabina`
+--
+
+CREATE TABLE `nave_tiene_cabina` (
+  `id_nave` int(11) NOT NULL,
+  `id_cabina` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pasajero`
+--
+
+CREATE TABLE `pasajero` (
+  `id_usuario` int(11) DEFAULT NULL,
+  `id_estado_fisico` int(11) DEFAULT NULL,
+  `id_hospital` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reserva`
+--
+
+CREATE TABLE `reserva` (
+  `id_reserva` int(11) NOT NULL,
+  `vencimiento_reserva` datetime DEFAULT NULL,
+  `id_estado_reserva` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reserva_cabina`
+--
+
+CREATE TABLE `reserva_cabina` (
+  `id_reserva` int(11) NOT NULL,
+  `id_cabina` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reserva_servicio`
+--
+
+CREATE TABLE `reserva_servicio` (
+  `id_reserva` int(11) NOT NULL,
+  `id_servicio` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `servicio`
+--
+
+CREATE TABLE `servicio` (
+  `id_servicio` int(11) NOT NULL,
+  `tipo_sevicio` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_usuario`
+--
+
+CREATE TABLE `tipo_usuario` (
+  `id_tipo_usuario` int(11) NOT NULL,
+  `descripcion` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_viaje`
+--
+
+CREATE TABLE `tipo_viaje` (
+  `id_tipo_viaje` int(11) NOT NULL,
+  `nombre_tipo_viaje` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `tipo_viaje`
+--
+
+INSERT INTO `tipo_viaje` (`id_tipo_viaje`, `nombre_tipo_viaje`) VALUES
+(1, 'orbital');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `turno_hospital`
+--
+
+CREATE TABLE `turno_hospital` (
+  `id_hospital` int(11) NOT NULL,
+  `nombre_hospital` varchar(35) DEFAULT NULL,
+  `turnos` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL,
+  `nombre` varchar(20) DEFAULT NULL,
+  `apellido` varchar(25) DEFAULT NULL,
+  `email` varchar(40) DEFAULT NULL,
+  `contraseña` varchar(40) DEFAULT NULL,
+  `id_tipo_usuario` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario_hace_reserva`
+--
+
+CREATE TABLE `usuario_hace_reserva` (
+  `id_reserva` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `viaje`
+--
+
+CREATE TABLE `viaje` (
+  `id_viaje` int(11) NOT NULL,
+  `salida_viaje` datetime DEFAULT NULL,
+  `llegada_viaje` datetime DEFAULT NULL,
+  `duracion` time DEFAULT NULL,
+  `precio` int(11) DEFAULT NULL,
+  `id_tipo_viaje` int(11) DEFAULT NULL,
+  `id_lugar_destino` int(11) DEFAULT NULL,
+  `id_lugar_origen` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `viaje`
+--
+
+INSERT INTO `viaje` (`id_viaje`, `salida_viaje`, `llegada_viaje`, `duracion`, `precio`, `id_tipo_viaje`, `id_lugar_destino`, `id_lugar_origen`) VALUES
+(1, '2019-10-09 00:00:00', '2019-10-17 00:00:00', '05:00:00', 123, 1, 1, 1),
+(2, '2019-10-17 00:00:00', '2019-10-18 00:00:00', '05:00:00', 456, 1, 2, 2),
+(3, '2019-10-16 00:00:00', '2019-10-19 00:00:00', '09:00:00', 567, 1, 1, 1),
+(4, '2019-11-14 00:00:00', '2019-11-16 00:00:00', '12:00:00', 4562, 1, 1, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `viaje_hecho_por`
+--
+
+CREATE TABLE `viaje_hecho_por` (
+  `id_reserva` int(11) NOT NULL,
+  `id_viaje` int(11) NOT NULL,
+  `id_nave` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `viaje_puede_ser_hecho_por`
+--
+
+CREATE TABLE `viaje_puede_ser_hecho_por` (
+  `id_estado_fisico` int(11) NOT NULL,
+  `id_viaje` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cabina`
+--
+ALTER TABLE `cabina`
+  ADD PRIMARY KEY (`id_cabina`);
+
+--
+-- Indices de la tabla `estado_fisico`
+--
+ALTER TABLE `estado_fisico`
+  ADD PRIMARY KEY (`id_estado_fisico`);
+
+--
+-- Indices de la tabla `estado_reserva`
+--
+ALTER TABLE `estado_reserva`
+  ADD PRIMARY KEY (`id_estado_reserva`);
+
+--
+-- Indices de la tabla `lugar`
+--
+ALTER TABLE `lugar`
+  ADD PRIMARY KEY (`id_lugar`);
+
+--
+-- Indices de la tabla `nave`
+--
+ALTER TABLE `nave`
+  ADD PRIMARY KEY (`id_nave`);
+
+--
+-- Indices de la tabla `nave_tiene_cabina`
+--
+ALTER TABLE `nave_tiene_cabina`
+  ADD PRIMARY KEY (`id_nave`,`id_cabina`),
+  ADD KEY `id_cabina` (`id_cabina`);
+
+--
+-- Indices de la tabla `pasajero`
+--
+ALTER TABLE `pasajero`
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_estado_fisico` (`id_estado_fisico`),
+  ADD KEY `id_hospital` (`id_hospital`);
+
+--
+-- Indices de la tabla `reserva`
+--
+ALTER TABLE `reserva`
+  ADD PRIMARY KEY (`id_reserva`),
+  ADD KEY `id_estado_reserva` (`id_estado_reserva`);
+
+--
+-- Indices de la tabla `reserva_cabina`
+--
+ALTER TABLE `reserva_cabina`
+  ADD PRIMARY KEY (`id_reserva`,`id_cabina`),
+  ADD KEY `id_cabina` (`id_cabina`);
+
+--
+-- Indices de la tabla `reserva_servicio`
+--
+ALTER TABLE `reserva_servicio`
+  ADD PRIMARY KEY (`id_reserva`,`id_servicio`),
+  ADD KEY `id_servicio` (`id_servicio`);
+
+--
+-- Indices de la tabla `servicio`
+--
+ALTER TABLE `servicio`
+  ADD PRIMARY KEY (`id_servicio`);
+
+--
+-- Indices de la tabla `tipo_usuario`
+--
+ALTER TABLE `tipo_usuario`
+  ADD PRIMARY KEY (`id_tipo_usuario`);
+
+--
+-- Indices de la tabla `tipo_viaje`
+--
+ALTER TABLE `tipo_viaje`
+  ADD PRIMARY KEY (`id_tipo_viaje`);
+
+--
+-- Indices de la tabla `turno_hospital`
+--
+ALTER TABLE `turno_hospital`
+  ADD PRIMARY KEY (`id_hospital`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD KEY `id_tipo_usuario` (`id_tipo_usuario`);
+
+--
+-- Indices de la tabla `usuario_hace_reserva`
+--
+ALTER TABLE `usuario_hace_reserva`
+  ADD PRIMARY KEY (`id_reserva`,`id_usuario`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `viaje`
+--
+ALTER TABLE `viaje`
+  ADD PRIMARY KEY (`id_viaje`),
+  ADD KEY `id_tipo_viaje` (`id_tipo_viaje`),
+  ADD KEY `id_lugar` (`id_lugar_destino`),
+  ADD KEY `id_lugar_origen` (`id_lugar_origen`);
+
+--
+-- Indices de la tabla `viaje_hecho_por`
+--
+ALTER TABLE `viaje_hecho_por`
+  ADD PRIMARY KEY (`id_reserva`,`id_viaje`,`id_nave`),
+  ADD KEY `id_viaje` (`id_viaje`),
+  ADD KEY `id_nave` (`id_nave`);
+
+--
+-- Indices de la tabla `viaje_puede_ser_hecho_por`
+--
+ALTER TABLE `viaje_puede_ser_hecho_por`
+  ADD PRIMARY KEY (`id_estado_fisico`,`id_viaje`),
+  ADD KEY `id_viaje` (`id_viaje`);
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `nave_tiene_cabina`
+--
+ALTER TABLE `nave_tiene_cabina`
+  ADD CONSTRAINT `nave_tiene_cabina_ibfk_1` FOREIGN KEY (`id_nave`) REFERENCES `nave` (`id_nave`),
+  ADD CONSTRAINT `nave_tiene_cabina_ibfk_2` FOREIGN KEY (`id_cabina`) REFERENCES `cabina` (`id_cabina`);
+
+--
+-- Filtros para la tabla `pasajero`
+--
+ALTER TABLE `pasajero`
+  ADD CONSTRAINT `pasajero_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`),
+  ADD CONSTRAINT `pasajero_ibfk_2` FOREIGN KEY (`id_estado_fisico`) REFERENCES `estado_fisico` (`id_estado_fisico`),
+  ADD CONSTRAINT `pasajero_ibfk_3` FOREIGN KEY (`id_hospital`) REFERENCES `turno_hospital` (`id_hospital`);
+
+--
+-- Filtros para la tabla `reserva`
+--
+ALTER TABLE `reserva`
+  ADD CONSTRAINT `reserva_ibfk_1` FOREIGN KEY (`id_estado_reserva`) REFERENCES `estado_reserva` (`id_estado_reserva`);
+
+--
+-- Filtros para la tabla `reserva_cabina`
+--
+ALTER TABLE `reserva_cabina`
+  ADD CONSTRAINT `reserva_cabina_ibfk_1` FOREIGN KEY (`id_reserva`) REFERENCES `reserva` (`id_reserva`),
+  ADD CONSTRAINT `reserva_cabina_ibfk_2` FOREIGN KEY (`id_cabina`) REFERENCES `cabina` (`id_cabina`);
+
+--
+-- Filtros para la tabla `reserva_servicio`
+--
+ALTER TABLE `reserva_servicio`
+  ADD CONSTRAINT `reserva_servicio_ibfk_1` FOREIGN KEY (`id_reserva`) REFERENCES `reserva` (`id_reserva`),
+  ADD CONSTRAINT `reserva_servicio_ibfk_2` FOREIGN KEY (`id_servicio`) REFERENCES `servicio` (`id_servicio`);
+
+--
+-- Filtros para la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`id_tipo_usuario`) REFERENCES `tipo_usuario` (`id_tipo_usuario`);
+
+--
+-- Filtros para la tabla `usuario_hace_reserva`
+--
+ALTER TABLE `usuario_hace_reserva`
+  ADD CONSTRAINT `usuario_hace_reserva_ibfk_1` FOREIGN KEY (`id_reserva`) REFERENCES `reserva` (`id_reserva`),
+  ADD CONSTRAINT `usuario_hace_reserva_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`);
+
+--
+-- Filtros para la tabla `viaje`
+--
+ALTER TABLE `viaje`
+  ADD CONSTRAINT `viaje_ibfk_1` FOREIGN KEY (`id_tipo_viaje`) REFERENCES `tipo_viaje` (`id_tipo_viaje`),
+  ADD CONSTRAINT `viaje_ibfk_2` FOREIGN KEY (`id_lugar_destino`) REFERENCES `lugar` (`id_lugar`),
+  ADD CONSTRAINT `viaje_ibfk_3` FOREIGN KEY (`id_lugar_origen`) REFERENCES `lugar` (`id_lugar`);
+
+--
+-- Filtros para la tabla `viaje_hecho_por`
+--
+ALTER TABLE `viaje_hecho_por`
+  ADD CONSTRAINT `viaje_hecho_por_ibfk_1` FOREIGN KEY (`id_reserva`) REFERENCES `reserva` (`id_reserva`),
+  ADD CONSTRAINT `viaje_hecho_por_ibfk_2` FOREIGN KEY (`id_viaje`) REFERENCES `viaje` (`id_viaje`),
+  ADD CONSTRAINT `viaje_hecho_por_ibfk_3` FOREIGN KEY (`id_nave`) REFERENCES `nave` (`id_nave`);
+
+--
+-- Filtros para la tabla `viaje_puede_ser_hecho_por`
+--
+ALTER TABLE `viaje_puede_ser_hecho_por`
+  ADD CONSTRAINT `viaje_puede_ser_hecho_por_ibfk_1` FOREIGN KEY (`id_estado_fisico`) REFERENCES `estado_fisico` (`id_estado_fisico`),
+  ADD CONSTRAINT `viaje_puede_ser_hecho_por_ibfk_2` FOREIGN KEY (`id_viaje`) REFERENCES `viaje` (`id_viaje`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
